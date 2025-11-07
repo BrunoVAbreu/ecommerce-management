d62ad2bf59ca495444e7d78a166c17b29be31028
Bruno Abreu <vazs.bruno@gmail.com>
2025-11-07 14:58:21 -0300

    feat: User service

--- src/main/java/com/brunovaz/ecommerce/resources/UserResource.java
+++ src/main/java/com/brunovaz/ecommerce/resources/UserResource.java
@@ -1,20 +1,37 @@
 package com.brunovaz.ecommerce.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brunovaz.ecommerce.entities.User;
+import com.brunovaz.ecommerce.services.UserService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
 @RequestMapping ("/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "Maria@gmail.com", "9999999", "1234");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    
 }
